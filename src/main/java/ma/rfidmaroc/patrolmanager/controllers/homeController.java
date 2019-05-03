91703be6161f63528b7f78d2a534ed7698278ebb
BAROUCH Ayoub <hassanazan3@gmail.com>
2019-05-03 03:33:52 +0200

    ajouter un layout pour les vues ..

--- src/main/java/ma/rfidmaroc/patrolmanager/controllers/homeController.java
+++ src/main/java/ma/rfidmaroc/patrolmanager/controllers/homeController.java
@@ -1,22 +1,10 @@
 package ma.rfidmaroc.patrolmanager.controllers;
 
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import ma.rfidmaroc.patrolmanager.dao.UtilisateurRepository;
-import ma.rfidmaroc.patrolmanager.entities.Utilisateur;
 
 @Controller
 public class homeController {
-	
-	@Autowired
-	private UtilisateurRepository utilisateurRepository;
 	
 	@RequestMapping(value="/home")
 	public String homeController() {
@@ -31,21 +19,5 @@ public class homeController {
 	@RequestMapping(value="/login")
 	public String loginController() {
 		return "login";
-	}
-	
-	@RequestMapping(value="/users", method=RequestMethod.GET)
-	public String usersController(Model model) {
-		model.addAttribute("utilisateur", new Utilisateur());
-		return "users";
-	}
-	
-	@RequestMapping(value="/SaveUtilisateur", method=RequestMethod.POST)
-	public String save(@Valid Utilisateur user, BindingResult bindingResult) {
-		if(bindingResult.hasErrors())
-		{
-			return "users";
-		}
-		utilisateurRepository.save(user);
-		return "users";
 	}
 }
